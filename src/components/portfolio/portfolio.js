a4c1bacf4d46042e6edc180ec1c09881a1405a02
Nick Sundermeyer <nicksundermeyer@gmail.com>
2020-01-13 10:03:46 -0700

    Added videos, cleared warnings

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import Modal from './portfolio-modal';
-import portfolioData from '../../portfolioData';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import { FormControl, withStyles } from '@material-ui/core';
+import React, { Component } from "react";
+import Modal from "./portfolio-modal";
+import portfolioData from "../../portfolioData";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import { FormControl, withStyles } from "@material-ui/core";
 
 const styles = theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   formControl: {
     margin: theme.spacing.unit,
-    minWidth: 120,
-  },
+    minWidth: 120
+  }
 });
 
 class Portfolio extends Component {
   state = {
-    category: ''
-  }
+    category: ""
+  };
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
@@ -37,22 +36,36 @@ class Portfolio extends Component {
             <form className={classes.root} autoComplete="off">
               <FormControl className={classes.formControl}>
                 {/* <InputLabel htmlFor="age-simple">Filter</InputLabel> */}
-                <Select value={this.state.category} onChange={this.handleChange} inputProps={{ name: 'category', id: "age-simple"}}>
-                  <MenuItem value=""><em>Filter</em></MenuItem>
-                  <MenuItem value={"Game Development"}>Game Development</MenuItem>
+                <Select
+                  value={this.state.category}
+                  onChange={this.handleChange}
+                  inputProps={{ name: "category", id: "age-simple" }}
+                >
+                  <MenuItem value="">
+                    <em>Filter</em>
+                  </MenuItem>
+                  <MenuItem value={"Game Development"}>
+                    Game Development
+                  </MenuItem>
                   <MenuItem value={"Graphic Design"}>Graphic Design</MenuItem>
                   <MenuItem value={"3D Work"}>3D Work</MenuItem>
                   <MenuItem value={"Web Development"}>Web Development</MenuItem>
                 </Select>
               </FormControl>
             </form>
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
               {portfolioData.map((e, i) => {
-                if (this.state.category == "") {
-                  return <Modal portfolioData={portfolioData[i]} />
-                }
-                else if (portfolioData[i].categories == this.state.category) {
-                  return <Modal portfolioData={portfolioData[i]} />
+                if (this.state.category === "") {
+                  return <Modal portfolioData={portfolioData[i]} />;
+                } else if (
+                  portfolioData[i].categories === this.state.category
+                ) {
+                  return <Modal portfolioData={portfolioData[i]} />;
+                } else {
+                  return null;
                 }
               })}
             </div>
